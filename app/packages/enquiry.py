39e33fd727d69f52516f4ef830d647fc76f0afd5
Komal-2108 <thekomal2502@gmail.com>
2025-12-28 19:05:13 +0530

    Update tamia file

--- app/packages/enquiry.py
+++ app/packages/enquiry.py
@@ -5,7 +5,7 @@ from app.database import engine , get_db
 from app.config import settings  
 from app.utils.mail.enquiry_mail import send_enquiry_email
 import shutil, os
-from fastapi import C
+# from fastapi import C
 from app.utils.invoice_generator import generate_invoice
 from datetime import date
 
